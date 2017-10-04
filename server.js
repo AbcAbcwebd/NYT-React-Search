0e2282a2210f3371b648e5d8254edeb7f23691d1
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-04 13:58:11 -0400

    Trying to get files up and running.

---
+++ server.js
@@ -0,0 +1,20 @@
+// Boilerplate code taken from in class demonstration 
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("nyt-react-search/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./nyt-react-search/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
