020412818f8dff30ae860a576815ad76a6ffe06e
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-07 11:34:36 -0400

    Header component created.

--- nyt-react-search/src/App.js
+++ nyt-react-search/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Siteheader from './components/header';
 
 class App extends Component {
   render() {
     return (
-      <p>Test 3</p>
+      <Siteheader />
+//		<p>Stuff from the App Component</p>
     );
   }
 }
---
+++ nyt-react-search/src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './header.css';
+
+const Siteheader = () => 
+  <div>
+  	<h1>New York Times Article Scrubber</h1>
+    <p>Search for and annotate articles of interest!</p>
+  </div>;
+
+
+export default Siteheader;
