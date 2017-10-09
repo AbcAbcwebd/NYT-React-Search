338b9e695cc564b009612be8663a8d51ff8e6a20
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-09 11:33:09 -0400

    Article display system fleshed out.

--- nyt-react-search/src/components/articleThumbnail/articleThumbnail.js
+++ nyt-react-search/src/components/articleThumbnail/articleThumbnail.js
@@ -5,10 +5,11 @@ const testLink = "http://www.google.com"
 
 const ArticleThumbnail = props => (
   <div className="articleThumbnail">
-    <a href={testLink}>
+    <a href={props.link} target="_blank">
       <h3>{props.headline}</h3>
       <p className="byLine">{props.byLine}</p>
       <p>{props.summary}</p>
+      <button>Save</button>
     </a>
   </div>
 );
